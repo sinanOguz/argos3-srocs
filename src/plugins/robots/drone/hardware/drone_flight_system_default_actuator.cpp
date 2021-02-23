4f7f82d744d9d9d930c86a940be5dcfe9ee9abaf
sinanOguz <soguz.ankara@gmail.com>
2021-02-23 12:35:58 +0100

    update

--- src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
+++ src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
@@ -74,7 +74,7 @@ namespace argos {
             m_pcPixhawk->GetInitialPosition().value();
          uint8_t unTargetSystem =
             m_pcPixhawk->GetTargetSystem().value();
-         CVector3 fTargetPosition = m_cTargetPosition.RotateZ(cInitialOrientation.GetZ());
+         CVector3& fTargetPosition = m_cTargetPosition.RotateZ(cInitialOrientation.GetZ());
          /* initialize a setpoint struct */
          mavlink_set_position_target_local_ned_t tSetpoint;
          tSetpoint.target_system    = m_pcPixhawk->GetTargetSystem().value();
